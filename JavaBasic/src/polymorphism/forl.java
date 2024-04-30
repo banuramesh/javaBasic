7596ab4ca9f54f122fd2ee2380187f8ce77f3887
Rameshbabu <Rameshbabu@Rameshbabu>
2024-04-30 15:51:10 +0530

    initial commit

---
+++ JavaBasic/src/polymorphism/forl.java
@@ -0,0 +1,22 @@
+package polymorphism;
+
+public class forl {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<= 170; i=i+17)
+		{
+			System.out.println(i);
+		}
+		
+		for(int c = 20; c<=30; c=c+1)
+		{
+			System.out.println(c);
+		}
+		
+		
+		
+		
+	}
+
+}
